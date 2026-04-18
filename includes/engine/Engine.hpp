77fd72dd826da5e363de05b9c92ab64acf61e26e
ssawa <sw20050719@gmail.com>
2026-04-18 18:49:57 +0900

    update Engine.hpp

---
+++ includes/engine/Engine.hpp
@@ -0,0 +1,15 @@
+#ifndef ENGINE_HPP
+# define ENGINE_HPP
+
+# include <string>
+# include <map>
+
+class Engine
+{
+	public:
+
+	private:
+		std::multimap<std::string, std::string>	transcactions;
+};
+
+#endif
