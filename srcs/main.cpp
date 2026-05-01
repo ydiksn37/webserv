2484e78a27102a2c2b74e22e6598ac674098bdd8
Yudai Kusano <yukusano@c6r1s1.42tokyo.jp>
2026-05-01 21:09:20 +0900

    modify main.cpp

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
 #include "Config.hpp"
 #include "EventLoop.hpp"
 
-int main(int argc, char** argv)
-{
-	if(argc != 2)
-		std::cerr<<"Invalid argument."<<std::endl;
+int main(int argc, char** argv) {
+	std::string config_file;
+
+	if (argc == 2) {
+		config_file = argv[1];
+	} else if (argc == 1) {
+		config_file = "configurations/default.conf";
+		std::cout << "[Info] No configuration file specified. Using default: " << config_file << std::endl;
+	} else {
+		std::cerr << "Usage: ./webserv [config_file]" << std::endl;
+		return 1;
+	}
+
 	Config config;
 
 	try {
-		config.loadFile(argv[1]);
+		config.loadFile(config_file);
 	} catch (const Config::ConfigException& e) {
 		std::cerr << "\n[Config Error] " << e.what() << std::endl;
 		return 1;
@@ -21,7 +29,9 @@ int main(int argc, char** argv)
 	try {
 		EventLoop(config);
 	} catch (const std::exception& e) {
-		std::cerr << "\n[Error]" << e.what() << std::endl;
+		std::cerr << "\n[Error] " << e.what() << std::endl;
 		return 1;
 	}
+
+	return 0;
 }
